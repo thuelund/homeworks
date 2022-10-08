53457aa32fcbc8391d5c2ca1f6a99a56262ffea7
thuelund <thuelund@outlook.com>
2022-10-08 21:08:11 +0200

    Completed homework 2, and restructured homeworks

---
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <array>
+
+int main(){
+    const std::map<std::string, std::string> season_map{{"spring", "STL guru"},
+                                                        {"summer", "C++ expert"},
+                                                        {"autumn", "coding beast"},
+                                                        {"winter", "software design hero"}};
+    
+    const std::array ending{"eats UB for breakfast", "finds errors quicker than the compiler", "is not afraid of C++ error messages"};
+
+    std::string name{};
+    std::string season{};
+    std::string tmp{};
+    std::vector<std::string> adj = {};
+    int idx1 {};
+    int idx2 {};
+
+    std::cout << "Welcome to the fortune teller program!" << std::endl;
+    std::cout << "Please enter your name:" << std::endl;
+    std::cin >> name;
+    std::cout << "Please enter the time of year when you were born:" << std::endl;
+    std::cout << "(pick from 'spring', 'summer', 'autumn', 'winter')" << std::endl;
+    std::cin >> season;
+    std::cout << "Please enter an adjective:" << std::endl;
+    std::cin >> tmp;
+    adj.reserve(adj.size() + 1UL);
+    adj.emplace_back(tmp);
+    std::cout << "Please enter another adjective:" << std::endl;
+    std::cin >> tmp;
+    adj.reserve(adj.size() + 1UL);
+    adj.emplace_back(tmp);
+
+    idx1 = name.size() % adj.size();
+    idx2 = name.size() % ending.size();
+
+    std::cout << name << ", the " << adj[idx1] << " " << season_map.at(season) << " that " << ending[idx2] << std::endl;
+    return 0;
+}
